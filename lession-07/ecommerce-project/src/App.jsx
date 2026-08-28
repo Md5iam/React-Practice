f911ec0288187b4ee57a29b91f5da34654f0a15c
md5iam <mdsiam9837@gmail.com>
2026-08-28 18:53:14 +0600

    React with Backend, Data Fetching, Load Ecommerce Project Data

--- lession-07/ecommerce-project/src/App.jsx
+++ lession-07/ecommerce-project/src/App.jsx
@@ -1,28 +1,28 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import {Routes , Route} from 'react-router'
-import { HomePage } from './assets/pages/HomePage'
-import {CheckoutPage} from './assets/pages/CheckoutPage'
-import { OrdersPage } from './assets/pages/OrdersPage'
-
+import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
 
-  const[cart, setCart] = useState([]);
-
-  useEffect(()=>{
-    axios.get('http://localhost:3000/api/cart-items')
-    .then((response)=>{
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
       setCart(response.data);
-    })
-  },[])
+    };
+
+    fetchAppData();
+  }, []);
 
   return (
     <Routes>
-      <Route path="/" element = {<HomePage cart={cart} />} />
-      <Route path="/checkout" element = {<CheckoutPage cart={cart} />} />
-      <Route path="/orders" element = {<OrdersPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   )
 }
