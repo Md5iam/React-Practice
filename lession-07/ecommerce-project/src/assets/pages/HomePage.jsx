943ebbfb2114034ec6144353b224b7fd70558c22
md5iam <mdsiam9837@gmail.com>
2026-08-28 12:14:45 +0600

    Fetch Data from the backend

--- lession-07/ecommerce-project/src/assets/pages/HomePage.jsx
+++ lession-07/ecommerce-project/src/assets/pages/HomePage.jsx
@@ -3,6 +3,15 @@ import { Header } from '../../components/Header';
 import { products } from '../../../starting-code/data/products.js';
 
 export function HomePage() {
+ 
+  // get data form backend
+  fetch('http://localhost:3000/api/products')
+  .then((response)=>{
+    return response.json();
+  }).then((data)=>{
+    console.log(data);
+  })
+
   return (
     <>
       <title>Ecommerce Project</title>
